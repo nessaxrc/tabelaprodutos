546ce8ad4c3c659e26f566616b4df6951e99578f
nessaxrc <149071276+nessaxrc@users.noreply.github.com>
2023-10-29 16:28:06 -0300

    botão nv

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const btnNovo = document.querySelector(".btnNovo");
+const tela = document.querySelector(".areaNovoProduto");
+const bloqueio  = document.querySelector(".block");
+const botaoCancelar = document.querySelector(".btnCancelar");
+
 function verificarEstoque(linha)
 {
     let valorQuantidade = Number(linha.children[2].textContent );
@@ -7,7 +12,7 @@ function verificarEstoque(linha)
     }
     else
     {
-        linha.children[4].innerHTML = "Regular";
+        linha.children[4].textContent = "Regular";
     }
 }
 
@@ -16,3 +21,34 @@ const linhasProdutos = document.querySelectorAll('.linha-produto');
 for (let i = 0; i < linhasProdutos.length; i++) {
     verificarEstoque( linhasProdutos[i]);
 }
+
+function ClicouBotaoAdicionar(event)
+{
+	event.preventDefault();
+
+	tela.classList.remove("escondido");
+	bloqueio.classList.remove("escondido");
+}
+
+btnNovo.addEventListener("click", ClicouBotaoAdicionar);
+
+
+// ---------------------------------------------------------------
+
+function cancelar(event)
+{
+	event.preventDefault();
+	sumir();
+}
+
+function sumir()
+{
+	tela.txtCodigo.value = "";
+    tela.txtNome.value = "";
+    tela.txtQtd.value = "";
+    tela.txtValor.value = "";
+	tela.classList.add("escondido");
+	bloqueio.classList.add("escondido");
+}
+
+botaoCancelar.addEventListener("click", cancelar);
